ea039f80a924a8a7aa28527eb009e91fa81fb8b8
singhabhi04 <abhishek.topa@gmail.com>
2022-12-27 20:36:02 +0530

    first commit

---
+++ src/test/java/com/tmb/tests/LoginPageTest.java
@@ -0,0 +1,14 @@
+package com.tmb.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+	
+	@Test
+	public void test1() {
+		WebDriver driver = new ChromeDriver();
+	}
+
+}
